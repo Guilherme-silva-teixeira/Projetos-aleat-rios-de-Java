30f8bf29da56782c6d451dd52320d2a2a8df7403
xX_ShakeDev[REAL]_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-01-20 22:00:47 -0300

    Create projeto01.java

---
+++ projeto01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class projeto01
+  {
+    public static void Main(String[] args)
+    {
+      Scanner get = new Scanner(System.in);
+      String nome = get.Nextline();
+      System.out.println("\nolá "+nome);
+    }
+  }
